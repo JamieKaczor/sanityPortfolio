fd953c6101948df39f1e09974daf51acc06d1b15
Jamie Kaczor <dignanjk@aol.com>
2021-08-31 12:40:28 -0400

    styling

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,20 +23,20 @@ export default function Post() {
     }, []);
 
     return (
-        <main className="bg-green-100 min-h-screen p-12">
+        <main className="bg-blue-100 min-h-screen p-12">
             <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center cursive">
-                    Blog Posts Page
+                    Blog Posts
                 </h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-                    Welcome to my page of blog posts
+                    Welcome to my blog posts page!
                 </h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData && postData.map((post, index) => (
                         <article>
                             <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                                 <span
-                                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-blue-400"
                                     key={index}
                                 >
                                     <img
